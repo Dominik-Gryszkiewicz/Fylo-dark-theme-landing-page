06ab79419f1986909a594b1d51f353a05f375c66
Dominik Gryszkiewicz <dominik.gryszkiewicz95@gmail.com>
2022-04-11 03:23:30 +0200

    adding styles for desktop versions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import {Navbar} from 'bootstrap'
+
+const emailInput = document.querySelector('.access__box__form__input')
+const submitBtn = document.querySelector('.access__box__form__btn')
+const emailError = document.querySelector('.access__box__form__error')
+
+function validateEmail(email) {
+    let reg = /\S+@\S+\.\S+/;
+    return reg.test(email);
+}
+
+submitBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    let emailInputValue = emailInput.value
+    if (!validateEmail(emailInputValue)) {
+        emailError.classList.add('active')
+    } else {
+        emailError.classList.remove('active')
+    }
+})
+
